f7a416f152be3eeed340741f0fbffbea4ce9c3d8
felixbor <felixbor@gmail.com>
2022-11-27 19:07:59 -0400

    api request

--- script.js
+++ script.js
@@ -3,19 +3,23 @@ var searchHistory =[];
 
 $(".btn").on("click", function(){
     var city = $("input").val()
-    $(".cities").append("<button>hhhhhhhh</buton>");
-$("<button>").val(searchHistory[i])
+    if ($("input").val()!=="")
+
+    $(".cities").append(`<button>${city}</button>`);
+
 addToHistory(city)
 console.log(city)
-
+fetchweather()
 
 })
 function addToHistory(city) {
     searchHistory.push(city);
     localStorage.setItem("search-history",JSON.stringify(searchHistory))
 }
-var q= "Toronto"
-fetch('https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={9305dd6618e87630258ad59d4733cd7f}')
+function fetchweather(){
+var cityname= $("input").val()
+console.log(cityname)
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=efb9260cdf2128e3c8f818c36c80e344&units=metric`)
   
 .then(function (response) {
     return response.json();
@@ -23,5 +27,14 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={9305
   .then(function (data) {
     console.log(data);
   });
+ }
+ /* fetch('api.openweathermap.org/data/2.5/forecast?q=Toronto&appid=efb9260cdf2128e3c8f818c36c80e344')
+    
+  .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
 
-  
+*/
