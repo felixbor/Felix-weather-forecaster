d66f91bc6c0412a49dbd514139fd8f00f186af7d
felixbor <felixbor@gmail.com>
2022-11-26 22:35:38 -0400

    aditing JS

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 var city = $("input").val()
+var searchHistory =[];
 
-$(".btn").on("click",function(){
+$(".btn").on("click", function(){
     var city = $("input").val()
-    $(".cities").append("<button></buton>");
+    $(".cities").append("<button>hhhhhhhh</buton>");
+$("<button>").val(searchHistory[i])
+addToHistory(city)
 console.log(city)
+
+
 })
+function addToHistory(city) {
+    searchHistory.push(city);
+    localStorage.setItem("search-history",JSON.stringify(searchHistory))
+}
+var q= "Toronto"
+fetch('https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={9305dd6618e87630258ad59d4733cd7f}')
+  
+.then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
+
+  
